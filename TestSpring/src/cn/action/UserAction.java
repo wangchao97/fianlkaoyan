718d5e767c5e6709701ad05ba78e897d336f9e11
wangchao97 <32935692+wangchao97@users.noreply.github.com>
2017-10-25 15:41:29 +0800

    Update UserAction.java

--- TestSpring/src/cn/action/UserAction.java
+++ TestSpring/src/cn/action/UserAction.java
@@ -15,7 +15,7 @@ public class UserAction {
 		this.userService = userService;
 	}
 	public String excute() {
-		int a=10;
+		
 		System.out.println("excuteִ��");
 		userService.save();
 		return null;
